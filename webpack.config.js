0202acc17601c68e2221ea1671a6bfc9b8c17cd2
Fitarica <59100441+Fitarica@users.noreply.github.com>
2022-04-01 14:39:40 +0200

    webpack_2

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: [
+          {loader:"babel-loader"},
+        ]
+      },
+    ],
+  },
 };
